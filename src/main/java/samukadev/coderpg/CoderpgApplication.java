e3229f2e699b3db63010b6291bdec6dfdf3f3de8
Samuel Franklin <samuelfranklinff@gmail.com>
2025-11-03 19:20:42 -0300

    Initial commit

---
+++ src/main/java/samukadev/coderpg/CoderpgApplication.java
@@ -0,0 +1,13 @@
+package samukadev.coderpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/samukadev/coderpg/CoderpgApplicationTests.java
@@ -0,0 +1,13 @@
+package samukadev.coderpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
