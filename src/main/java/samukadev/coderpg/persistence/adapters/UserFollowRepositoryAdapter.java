e2d731c8604f5706dc1e3ee8f18bc81b69205b43
Samuel Franklin <samuelfranklinff@gmail.com>
2025-11-06 14:35:03 -0300

    feat: repository adapters

---
+++ src/main/java/samukadev/coderpg/persistence/adapters/UserFollowRepositoryAdapter.java
@@ -0,0 +1,7 @@
+package samukadev.coderpg.persistence.adapters;
+
+import samukadev.coderpg.core.persistence.UserFollowRepositoryPort;
+
+public class UserFollowRepositoryAdapter implements UserFollowRepositoryPort {
+
+}
---
+++ src/main/java/samukadev/coderpg/persistence/adapters/UserRepositoryAdapter.java
@@ -0,0 +1,60 @@
+package samukadev.coderpg.persistence.adapters;
+
+import static java.util.Optional.of;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import samukadev.coderpg.core.persistence.UserRepositoryPort;
+import samukadev.coderpg.core.persistence.commons.ReadRepositoryPort;
+import samukadev.coderpg.core.persistence.commons.WriteRepositoryPort;
+import samukadev.coderpg.domain.User;
+import samukadev.coderpg.persistence.mappers.UserMapper;
+import samukadev.coderpg.persistence.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional
+public class UserRepositoryAdapter implements UserRepositoryPort, ReadRepositoryPort<User>, WriteRepositoryPort<User> {
+
+    private final UserRepository repository;
+
+    private final UserMapper mapper;
+
+    @Override
+    public Optional<User> get(final UUID id) {
+        return repository.findById(id).map(mapper::map);
+    }
+
+    @Override
+    public User save(final User model) {
+        return of(repository.save(mapper.map(model)))
+                .map(mapper::map)
+                .orElseThrow(() -> new IllegalStateException("Failed to save user"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return of(repository.findAll())
+                .orElse(new ArrayList<>())
+                .stream()
+                .map(mapper::map).toList();
+    }
+
+    @Override
+    public Optional<User> findByEmail(final String email) {
+        return repository.findByEmail(email)
+                .map(mapper::map);
+    }
+
+    @Override
+    public boolean existsByEmail(final String email) {
+        return repository.existsByEmail(email);
+    }
+
+}
