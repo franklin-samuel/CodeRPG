0eeeedc21550d157532766e2e9eaa23654754709
Samuel Franklin <samuelfranklinff@gmail.com>
2025-11-06 17:06:57 -0300

    Create BusinessException.java

---
+++ src/main/java/samukadev/coderpg/domain/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package samukadev.coderpg.domain.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(final String message) {
+        super(message);
+    }
+
+}
