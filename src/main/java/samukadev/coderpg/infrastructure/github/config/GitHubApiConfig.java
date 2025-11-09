2b81629e21f17cfe8170c775f8f682a675122488
Samuel Franklin <samuelfranklinff@gmail.com>
2025-11-09 18:48:19 -0300

    refactor: github config

--- src/main/java/samukadev/coderpg/infrastructure/github/config/GitHubApiConfig.java
+++ src/main/java/samukadev/coderpg/infrastructure/github/config/GitHubApiConfig.java
@@ -5,27 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
 public class GitHubApiConfig {
 
     private final GitHubProperties gitHubProperties;
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
 
     @Bean
     public WebClient gitHubWebClient() {
-        HttpClient httpClient = HttpClient.create()
-                .responseTimeout(Duration.ofMillis(gitHubProperties.getRequestTimeout()));
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new  ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
 
         return WebClient.builder()
                 .baseUrl(gitHubProperties.getApiUrl())
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .defaultHeader(HttpHeaders.AUTHORIZATION, gitHubProperties.getAuthorizationHeader())
+                .apply(oauth2.oauth2Configuration())
                 .defaultHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader("User-Agent", "CodeRPG-App")
--- src/main/java/samukadev/coderpg/infrastructure/github/config/GitHubProperties.java
+++ src/main/java/samukadev/coderpg/infrastructure/github/config/GitHubProperties.java
@@ -10,15 +10,10 @@ import org.springframework.context.annotation.Configuration;
 public class GitHubProperties {
 
     private String apiUrl;
-    private String token;
-    private String webhookSecret;
     private String appId;
+    private String webhookSecret;
     private Integer requestTimeout = 30000;
     private Integer maxRetries = 3;
     private Boolean enableWebhooks = true;
-
-    public String getAuthorizationHeader() {
-        return "Bearer " + token;
-    }
 
 }
